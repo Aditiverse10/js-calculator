d85e96d1fbe48a240039ca182576cdb6e19da83c
Aditiverse10 <aditi.mishra291520@gmail.com>
2025-10-16 08:51:47 +0530

    First commit: added calculator files

---
+++ script.js
@@ -0,0 +1,30 @@
+let input = document.getElementById("input");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
+      // Evaluate the expression
+      string = eval(string);
+      input.value = string;
+    }
+    else if (e.target.innerHTML == 'AC') {
+      // Clear all
+      string = "";
+      input.value = string;
+    }
+    else if (e.target.innerHTML == 'DEL') {
+      // Delete last character
+      string = string.slice(0, -1);
+      input.value = string;
+    }
+    else {
+      // Add character to string
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
